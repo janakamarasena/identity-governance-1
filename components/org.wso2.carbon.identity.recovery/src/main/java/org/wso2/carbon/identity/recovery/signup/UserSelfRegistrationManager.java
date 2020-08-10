183ab126fff987862e315aedec6e90558af952eb
Tharindu Bandara <tharindu.bandara.edu@gmail.com>
2020-08-10 21:35:51 +0530

    Fix userstore manager in the recovery notification

--- components/org.wso2.carbon.identity.recovery/src/main/java/org/wso2/carbon/identity/recovery/signup/UserSelfRegistrationManager.java
+++ components/org.wso2.carbon.identity.recovery/src/main/java/org/wso2/carbon/identity/recovery/signup/UserSelfRegistrationManager.java
@@ -786,8 +786,13 @@ public class UserSelfRegistrationManager {
 
         RealmService realmService = IdentityRecoveryServiceDataHolder.getInstance().getRealmService();
         try {
-            return realmService.getTenantUserRealm(IdentityTenantUtil.getTenantId(user.getTenantDomain()))
-                    .getUserStoreManager();
+            org.wso2.carbon.user.api.UserRealm tenantUserRealm = realmService.getTenantUserRealm(IdentityTenantUtil.
+                    getTenantId(user.getTenantDomain()));
+            if (IdentityUtil.getPrimaryDomainName().equals(user.getUserStoreDomain())) {
+                return tenantUserRealm.getUserStoreManager();
+            }
+            return ((org.wso2.carbon.user.core.UserStoreManager) tenantUserRealm.getUserStoreManager())
+                    .getSecondaryUserStoreManager(user.getUserStoreDomain());
         } catch (UserStoreException e) {
             if (log.isDebugEnabled()) {
                 String message = String
